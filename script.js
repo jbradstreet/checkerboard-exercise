fcef79a804d4639ada361c5e43d1aa9fb9d49de8
Jenni Bradstreet <jennic15@gmail.com>
2016-02-23 09:22:00 -0800

    Make colors flash

--- script.js
+++ script.js
@@ -1,33 +1,43 @@
 // Your JS goes here
 
 
-var wrapFirstRow = document.createElement('section');
-var body = document.getElementsByTagName('body')[0];
-body.appendChild(wrapFirstRow);
-wrapFirstRow.style.width = "450px";
-wrapFirstRow.style.height = '450px';
+// var wrapFirstRow = document.createElement('section');
+// var body = document.getElementsByTagName('body')[0];
+// body.appendChild(wrapFirstRow);
+// wrapFirstRow.style.width = "450px";
+// wrapFirstRow.style.height = '450px';
 
 
 function chooseColor() {
   return 'rgb(' + (Math.floor(Math.random() * 255)) + ',' + (Math.floor(Math.random() * 255)) + ',' + (Math.floor(Math.random() * 255)) + ')';
 };
-setInterval(chooseColor, 2000);
 
 
-for(var i = 0; i < 81; i++){
-  var newDiv = document.createElement('div')
-  newDiv.style.cssText = 'width: 50px;';
-  newDiv.style.cssText += 'display: inline-block;';
-  newDiv.style.cssText += 'height: 50px;';
-  newDiv.style.cssText += 'margin-bottom: -4px;';
-      if (i % 2 == 0) {
-        newDiv.style.cssText += `background: ${chooseColor()}`;
-      } else {
-        newDiv.style.cssText += "background: " + chooseColor();
-      }
-    document.getElementsByTagName('section')[0]
-      .appendChild(newDiv)
-  }
+function flashing () {
+  var body = document.getElementsByTagName('body')[0];
+  body.innerHTML = '';
+  var wrapFirstRow = document.createElement('section');
+
+  body.appendChild(wrapFirstRow);
+  wrapFirstRow.style.width = "450px";
+  wrapFirstRow.style.height = '450px';
+  for(var i = 0; i < 81; i++){
+    var newDiv = document.createElement('div')
+    newDiv.style.cssText = 'width: 50px;';
+    newDiv.style.cssText += 'display: inline-block;';
+    newDiv.style.cssText += 'height: 50px;';
+    newDiv.style.cssText += 'margin-bottom: -4px;';
+        if (i % 2 == 0) {
+          newDiv.style.cssText += `background: ${chooseColor()}`;
+        } else {
+          newDiv.style.cssText += "background: " + chooseColor();
+        }
+      document.getElementsByTagName('section')[0]
+        .appendChild(newDiv)
+    }
+}
+flashing();
+setInterval(flashing, 2000);
 
 
 // var addButton = document.createElement('button');
