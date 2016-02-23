51f5ae4bbd0b715107bd772340572ce509cf1b36
Jenni Bradstreet <jennic15@gmail.com>
2016-02-22 22:40:35 -0800

    Remove codes for button, focus on timer

--- script.js
+++ script.js
@@ -7,6 +7,11 @@ body.appendChild(wrapFirstRow);
 wrapFirstRow.style.width = "450px";
 wrapFirstRow.style.height = '450px';
 
+
+function changeColor() {
+  setInterval(chooseColor, 1000);
+}
+
 function chooseColor() {
   return 'rgb(' + (Math.floor(Math.random() * 255)) + ',' + (Math.floor(Math.random() * 255)) + ',' + (Math.floor(Math.random() * 255)) + ')';
 }
@@ -27,10 +32,16 @@ for(var i = 0; i < 81; i++){
       .appendChild(newDiv)
   }
 
-var addButton = document.createElement('button');
-var body = document.getElementsByTagName('body')[0];
-body.appendChild(addButton);
 
-var addButtonText = document.createTextNode('Press to Start');
-var button = document.querySelector('button');
-button.appendChild(addButtonText);
+// var addButton = document.createElement('button');
+// var body = document.getElementsByTagName('body')[0];
+// body.appendChild(addButton);
+//
+// var addButtonText = document.createTextNode('Press to Start');
+// var button = document.querySelector('button');
+// button.appendChild(addButtonText);
+
+// var clickForFlashing = document.querySelector('button');
+// clickForFlashing.addEventListener('click', function () {
+//
+// });
