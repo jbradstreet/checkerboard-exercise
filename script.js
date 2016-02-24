ef030dacfb1976d6dce5dfb0f34d3617277634f0
Jenni Bradstreet <jennic15@gmail.com>
2016-02-24 12:06:00 -0800

    Modify code to remove eslint errors

--- script.js
+++ script.js
@@ -1,28 +1,27 @@
-'use strict';
+(function() {
+  'use strict';
+  var wrapFirstRow = document.createElement('section');
+  var body = document.getElementsByTagName('body')[0];
 
-var document = 'document';
-var wrapFirstRow = document.createElement('section');
-var body = document.getElementsByTagName('body')[0];
+  body.appendChild(wrapFirstRow);
+  wrapFirstRow.style.width = '450px';
+  wrapFirstRow.style.height = '450px';
 
-body.appendChild(wrapFirstRow);
-wrapFirstRow.style.width = '450px';
-wrapFirstRow.style.height = '450px';
+  for (var i = 0; i < 81; i++) {
+    var newDiv = document.createElement('div');
 
-for (var i = 0; i < 81; i++) {
-  var newDiv = document.createElement('div');
-
-  newDiv.style.cssText = 'width: 50px;';
-  newDiv.style.cssText += 'display: inline-block;';
-  newDiv.style.cssText += 'height: 50px;';
-  newDiv.style.cssText += 'margin-bottom: -4px;';
-  if (i % 2 === 0) {
-    newDiv.style.cssText += 'background: black';
+    newDiv.style.cssText = 'width: 50px;';
+    newDiv.style.cssText += 'height: 50px;';
+    newDiv.style.cssText += 'float: left;';
+    if (i % 2 === 0) {
+      newDiv.style.cssText += 'background: black';
+    }
+    else {
+      newDiv.style.cssText += 'background: red';
+    }
+    document.getElementsByTagName('section')[0]
+      .appendChild(newDiv);
   }
-  else {
-    newDiv.style.cssText += 'background: red';
-  }
-  document.getElementsByTagName('section')[0]
-    .appendChild(newDiv);
-}
+})();
 
 // var oddDivs = document.querySelectorAll('div:nth-of-type(odd)');
