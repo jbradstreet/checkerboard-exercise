c0e05551b9d0d23a702082616aaacd966b87a136
Jenni Bradstreet <jennic15@gmail.com>
2016-02-24 11:53:16 -0800

    Edit file to clear eslint errors

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
-// Your JS goes here
+'use strict';
 
+var document = 'document';
 var wrapFirstRow = document.createElement('section');
 var body = document.getElementsByTagName('body')[0];
+
 body.appendChild(wrapFirstRow);
-wrapFirstRow.style.width = "450px";
+wrapFirstRow.style.width = '450px';
 wrapFirstRow.style.height = '450px';
 
+for (var i = 0; i < 81; i++) {
+  var newDiv = document.createElement('div');
 
-for(var i = 0; i < 81; i++){
-  var newDiv = document.createElement('div')
   newDiv.style.cssText = 'width: 50px;';
   newDiv.style.cssText += 'display: inline-block;';
   newDiv.style.cssText += 'height: 50px;';
   newDiv.style.cssText += 'margin-bottom: -4px;';
-    if (i % 2 == 0) {
-      newDiv.style.cssText += 'background: black';
-    } else {
-      newDiv.style.cssText += "background: red";
-    }
+  if (i % 2 === 0) {
+    newDiv.style.cssText += 'background: black';
+  }
+  else {
+    newDiv.style.cssText += 'background: red';
+  }
   document.getElementsByTagName('section')[0]
-    .appendChild(newDiv)
+    .appendChild(newDiv);
 }
 
-var oddDivs = document.querySelectorAll('div:nth-of-type(odd)');
+// var oddDivs = document.querySelectorAll('div:nth-of-type(odd)');
